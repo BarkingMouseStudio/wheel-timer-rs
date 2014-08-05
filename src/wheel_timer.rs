445c272a81b7c057f623e5753afcce0cfad71601
jimfleming <jimmy.fleming@gmail.com>
2014-08-04 17:15:31 -0700

    Bumping version

--- src/wheel_timer.rs
+++ src/wheel_timer.rs
@@ -1,12 +1,8 @@
-extern crate test;
-
-use test::Bencher;
-
 use std::mem;
 use std::fmt::{Show, Formatter, Result};
 use std::iter::AdditiveIterator;
 
-enum Node<T> {
+pub enum Node<T> {
   Cons(T, Box<Node<T>>),
   Nil
 }
@@ -70,7 +66,7 @@ impl<T> Iterator<Node<T>> for WheelTimer<T> {
 impl<T> WheelTimer<T> {
 
   // Creates a new timer with the specified max interval
-  fn new(maxInterval: uint) -> WheelTimer<T> {
+  pub fn new(maxInterval: uint) -> WheelTimer<T> {
     // Initialize the ring with Nil values
     let mut ring = Vec::with_capacity(maxInterval);
     for _ in range(0u, maxInterval) {
@@ -85,12 +81,12 @@ impl<T> WheelTimer<T> {
   }
 
   // Returns the number of items currently scheduled
-  fn size(&self) -> uint {
+  pub fn size(&self) -> uint {
     return self.ring.iter().map(|node| node.len()).sum()
   }
 
   // Schedules a new value, available after `ticks`
-  fn schedule(&mut self, ticks: uint, value: T) {
+  pub fn schedule(&mut self, ticks: uint, value: T) {
     // Compute the scheduled position in the wheel
     let index = (self.currentTick + ticks) % self.maxInterval;
 
@@ -102,7 +98,7 @@ impl<T> WheelTimer<T> {
   }
 
   // Tick the timer, returning the list of nodes at the spot
-  fn tick(&mut self) -> Node<T> {
+  pub fn tick(&mut self) -> Node<T> {
     // Get the node at the current tick in the wheel
     let node = mem::replace(self.ring.get_mut(self.currentTick), Nil);
 
@@ -112,110 +108,4 @@ impl<T> WheelTimer<T> {
     // Return the node that was in that spot
     return node
   }
-}
-
-#[test]
-fn wheel_timer_new_test() {
-  let timer = WheelTimer::<uint>::new(3);
-  assert!(timer.maxInterval == 3);
-  assert!(timer.ring.capacity() == 3);
-  assert!(timer.ring.len() == 3);
-}
-
-#[test]
-fn wheel_timer_schedule_test() {
-  let mut timer = WheelTimer::<&'static str>::new(10);
-  timer.schedule(3, "tick");
-
-  timer.tick();
-  timer.tick();
-  timer.tick();
-
-  let node = timer.tick();
-  assert!(node != Nil);
-
-  let val = match node {
-    Cons(val, _) => val,
-    Nil => ""
-  };
-  assert!(val == "tick");
-}
-
-#[test]
-fn wheel_timer_tick_test() {
-  let mut timer = WheelTimer::<uint>::new(10);
-
-  for i in range(0, 10) {
-    timer.schedule(i, i)
-  }
-
-  for i in range(0, 10) {
-    let node = timer.tick();
-    assert!(node != Nil);
-
-    let val = match node {
-      Cons(val, _) => val,
-      Nil => -1
-    };
-    assert!(val == i);
-  }
-}
-
-#[test]
-fn wheel_timer_size_test() {
-  let mut timer = WheelTimer::<uint>::new(10);
-
-  for i in range(0, 10) {
-    timer.schedule(i, i)
-  }
-
-  assert!(timer.size() == 10);
-
-  for _ in timer {
-  }
-
-  assert!(timer.size() == 0);
-}
-
-#[bench]
-fn bench_wheel_timer_drain(b: &mut Bencher) {
-  let maxInterval = 20;
-  let mut timer = WheelTimer::<uint>::new(maxInterval);
-
-  b.iter(|| {
-    // Fill
-    for j in range(0u, 100u) {
-      timer.schedule(j%maxInterval, j%maxInterval);
-    }
-
-    // Drain
-    for k in timer {
-      continue;
-    }
-  });
-}
-
-#[bench]
-fn bench_wheel_timer_fill(b: &mut Bencher) {
-  let maxInterval = 20;
-  let mut timer = WheelTimer::<uint>::new(maxInterval);
-  let mut i = 0;
-
-  b.iter(|| {
-    timer.schedule(i%maxInterval, i%maxInterval);
-    i = i + 1;
-  });
-}
-
-#[bench]
-fn bench_wheel_timer_fast(b: &mut Bencher) {
-  let maxInterval = 2;
-  let mut timer = WheelTimer::<uint>::new(maxInterval);
-  let mut i = 0;
-
-  b.iter(|| {
-    timer.schedule(i%maxInterval, i%maxInterval);
-    timer.tick();
-    i = i + 1;
-  });
 }
---
+++ tests/test_wheel_timer.rs
@@ -0,0 +1,107 @@
+extern crate test;
+extern crate wheel_timer;
+
+use test::Bencher;
+
+use wheel_timer::WheelTimer;
+use wheel_timer::Node;
+use wheel_timer::Cons;
+use wheel_timer::Nil;
+
+#[test]
+fn wheel_timer_schedule_test() {
+  let mut timer = WheelTimer::<&'static str>::new(10);
+  timer.schedule(3, "tick");
+
+  timer.tick();
+  timer.tick();
+  timer.tick();
+
+  let node = timer.tick();
+  assert!(node != Nil);
+
+  let val = match node {
+    Cons(val, _) => val,
+    Nil => ""
+  };
+  assert!(val == "tick");
+}
+
+#[test]
+fn wheel_timer_tick_test() {
+  let mut timer = WheelTimer::<uint>::new(10);
+
+  for i in range(0, 10) {
+    timer.schedule(i, i)
+  }
+
+  for i in range(0, 10) {
+    let node = timer.tick();
+    assert!(node != Nil);
+
+    let val = match node {
+      Cons(val, _) => val,
+      Nil => -1
+    };
+    assert!(val == i);
+  }
+}
+
+#[test]
+fn wheel_timer_size_test() {
+  let mut timer = WheelTimer::<uint>::new(10);
+
+  for i in range(0, 10) {
+    timer.schedule(i, i)
+  }
+
+  assert!(timer.size() == 10);
+
+  for _ in timer {
+  }
+
+  assert!(timer.size() == 0);
+}
+
+#[bench]
+fn bench_wheel_timer_drain(b: &mut Bencher) {
+  let maxInterval = 20;
+  let mut timer = WheelTimer::<uint>::new(maxInterval);
+
+  b.iter(|| {
+    // Fill
+    for j in range(0u, 100u) {
+      timer.schedule(j%maxInterval, j%maxInterval);
+    }
+
+    // Drain
+    for k in timer {
+      continue;
+    }
+  });
+}
+
+#[bench]
+fn bench_wheel_timer_fill(b: &mut Bencher) {
+  let maxInterval = 20;
+  let mut timer = WheelTimer::<uint>::new(maxInterval);
+  let mut i = 0;
+
+  b.iter(|| {
+    timer.schedule(i%maxInterval, i%maxInterval);
+    i = i + 1;
+  });
+}
+
+#[bench]
+fn bench_wheel_timer_fast(b: &mut Bencher) {
+  let maxInterval = 2;
+  let mut timer = WheelTimer::<uint>::new(maxInterval);
+  let mut i = 0;
+
+  b.iter(|| {
+    timer.schedule(i%maxInterval, i%maxInterval);
+    timer.tick();
+    i = i + 1;
+  });
+}
